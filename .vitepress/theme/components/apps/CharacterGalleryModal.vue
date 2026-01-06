67ce2e68babd0e47c8fc9693e445e535f6c8231d
cookie <kiku06358@gmail.com>
2026-01-06 11:21:07 +0900

    빠른 수정 업로드

--- .vitepress/theme/components/apps/CharacterGalleryModal.vue
+++ .vitepress/theme/components/apps/CharacterGalleryModal.vue
@@ -315,10 +315,10 @@ onMounted(() => {
 
 .image-container {
   position: relative;
-  max-width: 100%;
-  max-height: 100%;
+  width: 100%;
+  height: 100%;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 }
 
